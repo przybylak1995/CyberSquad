201b632ad088f480108cfeac4892d86581ae4386
Kristof Przybylak <84039435+przybylak1995@users.noreply.github.com>
2025-01-26 17:11:46 +0100

    new push

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,38 +1,28 @@
-const firstSet = [
-    "assets/image1.jpg",
-    "assets/image2.png",
-    "assets/image3.jpeg"
-];
-
-const secondSet = [
-    "assets/parentia.png",
-    "assets/genk.png",
-    "assets/marcan.jpeg"
-];
-
-let currentSet = 0;
-let sets = [firstSet, secondSet];
-
-function updateImages() {
-    const currentImages = sets[currentSet];
-
-    currentImages.forEach((imageSrc, index) => {
-        const imgElement = document.getElementById(`carousel_image${index + 1}`);
-        
-        // Voeg fade-out effect toe
-        imgElement.classList.add('fade-out');
-        
-        setTimeout(() => {
-            // Verander de afbeelding
-            imgElement.src = imageSrc;
-            
-            // Voeg fade-in effect toe
-            imgElement.classList.remove('fade-out');
-            imgElement.classList.add('fade-in');
-        }, 700); // Zorgt ervoor dat de afbeelding verandert na de fade-out
-    });
-
-    currentSet = (currentSet + 1) % sets.length;
-}
-
-setInterval(updateImages, 5000);
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 4, // Display 4 images at a time
+    spaceBetween: 10, // Space between images
+    loop: true, // Enable loop
+    autoplay: {
+      delay: 3000, // Auto slide every 3 seconds
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+      // Responsive breakpoints
+      1024: {
+        slidesPerView: 4, // For larger screens, display 4 slides
+      },
+      768: {
+        slidesPerView: 2, // For medium screens, display 2 slides
+      },
+      480: {
+        slidesPerView: 1, // For small screens, display 1 slide
+      },
+    },
+  });
